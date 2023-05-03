96bcc4f396f3d88d676d1b00d3bb6c8b35ac8e15
Ryzord <herrera_ruben1@hotmail.com>
2023-05-03 14:22:31 +0200

    Fixed abs.js

--- staff/ruben-herrera/maths/abs.js
+++ staff/ruben-herrera/maths/abs.js
@@ -1,6 +1,7 @@
 function absFunction(value) {
     // TODO: Implement me
-    return value * -1
+    return (value > 0) ? value * 1 : value * -1
+    
 }
 
 console.log(absFunction(2 - 7))
@@ -11,3 +12,5 @@ console.log(absFunction(2 * 10 * -9))
 
 console.log(absFunction(1.2345 - 9.8765))
 // 8.642
+
+console.log(absFunction(20 + 10))
