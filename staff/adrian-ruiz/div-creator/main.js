2f6642aa37826b65542a83b3650434e8a4e7819f
Adrian Ruiz <adrian.ruiz@singulardevs.com>
2023-05-04 21:18:36 +0200

    add basic chess js

---
+++ staff/adrian-ruiz/div-creator/main.js
@@ -0,0 +1,12 @@
+
+const filaPar = document.createElement('div')
+filaPar.classList.add('fila-par')
+
+console.log(filaPar)
+
+const tablero = document.getElementById('tablero')
+
+tablero.appendChild(filaPar)
+
+const casillaBlanca = document.createElement('div')
+casillaBlanca.classList.add('casilla-blanca')
